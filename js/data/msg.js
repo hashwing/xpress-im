a3fefa65f01079a75e314cd468d685b033c3ceaf
hashwing <1144205233@qq.com>
2019-04-03 23:16:59 +0800

    update msg list

---
+++ js/data/msg.js
@@ -0,0 +1,43 @@
+new Vue({
+	el: "#msgs-list",
+	data() {
+		return {
+			msgs: [
+				{
+					name: "韩一敏",
+					time: "昨天12:35",
+					count: 1,
+					first: "杀杀杀"
+				},
+				{
+					name: "han",
+				},
+				{
+					name: "han",
+				},
+				{
+					name: "han",
+				},
+				{
+					name: "han",
+				},
+				{
+					name: "han",
+				},
+				{
+					name: "han",
+				},
+				{
+					name: "han",
+				},
+				{
+					name: "han",
+				},
+				{
+					name: "han",
+				}
+			],
+			password: "",
+		}
+	}
+})
